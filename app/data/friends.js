ebc98ba643b0b09bde7f2404925db6762f9ad4cb
Luis Rishi Puno <LRPuno@gmail.com>
2018-09-17 14:08:48 -0400

    Added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -21,8 +21,71 @@ var friendArray = [
             4,
             1
         ]
+    },
+    {
+        "name": "Rex",
+        "photo": "http://www.dododex.com/media/creature/rex.png",
+        "scores": [
+            3,
+            5,
+            4,
+            3,
+            5,
+            2,
+            1,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        "name": "Jill",
+        "photo": "https://www.out.com/sites/out.com/files/2017/09/15/jill.jpeg",
+        "scores": [
+            3,
+            1,
+            2,
+            1,
+            3,
+            4,
+            1,
+            1,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Sam",
+        "photo": "https://pbs.twimg.com/profile_images/852113561014947844/Eecle29Z_400x400.jpg",
+        "scores": [
+            5,
+            5,
+            3,
+            5,
+            4,
+            5,
+            2,
+            2,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Draymond",
+        "photo": "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/6589.png&w=350&h=254",
+        "scores": [
+            4,
+            4,
+            1,
+            3,
+            2,
+            4,
+            5,
+            5,
+            1,
+            2
+        ]
     }
-
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
